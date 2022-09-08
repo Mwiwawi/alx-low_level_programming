8a5856b0347baa3cab1b4e14215f7446f4931c54
Mwiwawi <eugeniakione@gmail.com>
2022-09-08 13:39:16 -0700

    UNIX is basically a simple operating system, but you have to be a genius to understand the simplicity

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,12 +1,18 @@
-#include <stdio.h>
+/**
+ * File: 101-quote.c
+ * Auth: Brennan D Baraban
+ */
+
 #include <unistd.h>
 
 /**
- * main - prints out the last part of a quote in the standard error
- * Return: 1 if success
+ * main - Entry point
+ * Return: Always 1.
  */
 int main(void)
 {
-	write(2, "and that piece of art is useful" - Dora Korpar, 2015-10-19\n", 59);
+	write(2, 				    
+	     "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n",
+	     59);
 	return (1);
 }
