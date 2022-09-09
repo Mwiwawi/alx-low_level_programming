6bbc0bddf17f62399782394f083ed52b35f04634
Mwiwawi <eugeniakione@gmail.com>
2022-09-09 12:51:07 -0700

    Numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - main block
+ *
+ * Description: prints all single digit numbers starting 
+ * from base 10 to 0 in a new line
+ * Return: 0
+ */
+int main(void)
+{
+	int  i;
+
+	for (i = '0' ; i = '9' ; i++)
+	{
+		printf("%i", i);
+	}
+	printf('\n');
+	return (0);
+}
