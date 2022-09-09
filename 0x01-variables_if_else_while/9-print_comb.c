71532d741084903b78d9d5f718ffc860dfe42061
Mwiwawi <eugeniakione@gmail.com>
2022-09-09 14:16:34 -0700

    Patience, persistence and perspiration make an unbeatable combination for success

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+/**
+ * main - main block
+ *
+ * Description: prints all possible combinations of single-digit numbers
+ * numbers must be separated by , , followed by space
+ * numbers should be printed in ascending order
+ * you can only use the putchar function
+ * four times maximum
+ * Return: 0
+ */
+int main(void)
+{
+	int c;
+
+	for (c = '48' ; c <= '57' ; c++)
+	{
+		putchar(c);
+		if (c != '57')
+		{
+			putchar(',');
+			putchar(' ');
+		}
+	}
+	putchar('\n');
+	return (0);
+}
