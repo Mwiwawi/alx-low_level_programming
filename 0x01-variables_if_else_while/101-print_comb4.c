4d5a7c076a49d79fe2dccbe2f8ff6dc8260417f6
Mwiwawi <eugeniakione@gmail.com>
2022-09-12 12:54:48 -0700

    The success combination in business is: Do what you do better... and: do more of what you do...

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -19,35 +19,34 @@ int main(void)
 {
 	int a;
 	int b;
-	int c = 0;
+	int c;
 
-	while (c < 10)
+
+	for (a = 48; a <= 57; a++)
 	{
-		b = 0;
-		while (b < 10)
+		for (c = a + 1; c <= 57; c++)
 		{
-			a = 0;
-			while (a < 0)
+			for (b = c + 1; b <= 57; b++)
 			{
-				if (a != b && b != c && c < b && b < a)
-				{
-					 putchar('0' + c);
-					 putchar('0' + b);
-					 putchar('0' + a);
 
-					 if (a + b + c != 9 + 8 + 7)
-					 {
-						 putchar(',');
-						 putchar(' ');
-					 }
+				putchar(a);
+				putchar(c);
+				putchar(b);
+
+				if ((a == 55) && (c == a + 1) && (b == c + 1))
+				{
+					break;
 				}
 
-				a++;
+				putchar(',');
+				putchar(' ');
+
 			}
-			b++;
+
 		}
-		c++;
+
 	}
+
 	putchar('\n');
 	return (0);
 }
