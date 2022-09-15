9348aaf454fabbe4253d19c9868ca10860db1b5e
Mwiwawi <eugeniakione@gmail.com>
2022-09-15 15:30:21 -0700

    number

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -3,13 +3,12 @@
 
 /**
  * print_number - prints a number
- * @n: The number to print
+ * @n: the number to print
  */
 
 void print_number(int n)
 {
 	unsigned int num = n;
-
 	if (n < 0)
 	{
 		putchar('-');
