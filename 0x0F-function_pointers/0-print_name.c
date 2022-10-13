3e72e32e92486fad97c734f847c2a48d3e14385e
Mwiwawi <eugeniakione@gmail.com>
2022-10-13 21:35:30 +0300

    0. What's my name

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,4 +1,7 @@
-i/**
+#include <stdlib.h>
+#include "function_pointers.h"
+
+/**
  * print_name - function to print name
  *
  * @name: name to print
@@ -9,6 +12,8 @@ i/**
 
 void print_name(char *name, void (*f)(char *))
 {
-	if (name && f)
-		f(name);
+	if (!name || !f)
+		return;
+
+	f(name);
 }
